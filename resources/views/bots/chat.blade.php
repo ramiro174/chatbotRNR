cb8d4c3325236419f7ceb1026f17eab3c49bcfeb
Ramiro <ramiro174@gmail.com>
2024-08-01 12:41:50 -0600

    add start  ad image test

--- resources/views/bots/chat.blade.php
+++ resources/views/bots/chat.blade.php
@@ -17,7 +17,7 @@
             inputtext.placeholder = "Escribe aqui tu mensaje";
 
             inputtext.addEventListener("keydown", function(event) {
-              alert(event.code );
+              alert("codigo:",event.code );
                 //  event.preventDefault();
                 if (event.code === 'Enter') {
                      let div = document.getElementById("messageArea");
