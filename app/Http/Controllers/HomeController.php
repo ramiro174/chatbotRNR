bbe56136e3fd0a03fdacf3ccd9b6e2864bec92e6
Ramiro <ramiro174@gmail.com>
2024-08-01 10:40:59 -0600

    add start  ad image test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
             return view('welcome');
         }
         return view('index', [
-            'welcome' => 'Hola Soy Guetza, tu acompañante virtual, gracias por escribir, puedes escribir \"hola\" para comenzar',
+            'welcome' => 'Hola Soy Yolo, tu acompañante virtual, gracias por escribir, puedes escribir \"hola\" para comenzar',
         ]);
     }
 }
